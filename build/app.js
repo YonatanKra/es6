0dc26c2a1bfc30c1d788f934cb7ee5ab073c7bb1
yccteam <kra.yonatan@gmail.com>
2015-06-08 12:25:47 +0300

    Created an arrow function
    The arrow functions replaced the old-style callback function (compare
    the build and the app files to see the difference)

--- build/app.js
+++ build/app.js
@@ -1,3 +1,15 @@
 "use strict";
 var __moduleName = (void 0);
-console.log(("" + (1 + 1)));
+var point = {
+  name: "Some Point",
+  shout: function(message, callback) {
+    callback(message);
+  },
+  greeting: function() {
+    var $__0 = this;
+    this.shout("Hi, I am ", (function(message) {
+      return console.log(message + $__0.name);
+    }));
+  }
+};
+point.greeting();
--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log(`${1+1}`);
+var point = {
+	name: "Some Point",
+	
+	shout: function(message, callback){
+		callback(message);
+	},
+	greeting: function(){
+		this.shout("Hi, I am ", message => console.log(message + this.name));
+	}
+}
+
+point.greeting();
