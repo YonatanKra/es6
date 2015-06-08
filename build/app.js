f423e72b2f10522925001bbcf7b59e06b8cde203
yccteam <kra.yonatan@gmail.com>
2015-06-08 13:20:49 +0300

    Added destructuring examples
    Look at the different console groups to see the different kinds of
    destructuring available in ES6

--- build/app.js
+++ build/app.js
@@ -2,6 +2,7 @@
 var __moduleName = (void 0);
 var point = {
   name: "Some Point",
+  color: "blue",
   shout: function() {
     var message = arguments[$traceurRuntime.toProperty(0)] !== (void 0) ? arguments[$traceurRuntime.toProperty(0)] : "Hello, I am " + this.name;
     var callback = arguments[$traceurRuntime.toProperty(1)] !== (void 0) ? arguments[$traceurRuntime.toProperty(1)] : (function(message) {
@@ -16,5 +17,82 @@ var point = {
     }));
   }
 };
-point.greeting();
-point.shout();
+console.group("Just the name");
+var name = point.name;
+console.log(name);
+console.groupEnd();
+console.group("Name and color");
+var $__1 = point,
+    name = $__1.name,
+    color = $__1.color;
+console.log(name);
+console.log(color);
+console.groupEnd();
+console.group("Name and color different aliases");
+var $__1 = point,
+    pointName = $__1.name,
+    pointColor = $__1.color;
+console.log(pointName);
+console.log(pointColor);
+console.groupEnd();
+console.group("Pick certain elements from array");
+var myArray = [];
+{
+  try {
+    throw undefined;
+  } catch ($i) {
+    $i = 1;
+    for (; $i <= 10; $i++) {
+      try {
+        throw undefined;
+      } catch (i) {
+        i = $i;
+        try {
+          myArray.push(i);
+        } finally {
+          $i = i;
+        }
+      }
+    }
+  }
+}
+var $__1 = myArray,
+    firstItem = $__1[$traceurRuntime.toProperty(0)],
+    sixthItem = $__1[$traceurRuntime.toProperty(5)];
+console.log(firstItem);
+console.log(sixthItem);
+console.groupEnd();
+console.group("Pick certain elements from array and destructure their personal stuff");
+myArray = [];
+{
+  try {
+    throw undefined;
+  } catch ($i) {
+    $i = 1;
+    for (; $i <= 10; $i++) {
+      try {
+        throw undefined;
+      } catch (i) {
+        i = $i;
+        try {
+          myArray.push({
+            id: i,
+            myStuff: _.uniqueId('Item ' + i.toString())
+          });
+        } finally {
+          $i = i;
+        }
+      }
+    }
+  }
+}
+var $__1 = myArray,
+    firstItem = $__1[$traceurRuntime.toProperty(0)],
+    sixthItem = $__1[$traceurRuntime.toProperty(5)];
+function getMyStuff($__1) {
+  var myStuff = $__1.myStuff;
+  console.log(myStuff);
+}
+getMyStuff(firstItem);
+getMyStuff(sixthItem);
+console.groupEnd();
--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var point = {
 	name: "Some Point",
+	color: "blue",
 	
 	shout: function(message = "Hello, I am " + this.name, callback = message => console.log(message)){
 		callback(message);
@@ -9,5 +10,45 @@ var point = {
 	}
 }
 
-point.greeting();
-point.shout();
+console.group("Just the name");
+let {name} = point;
+console.log(name);
+console.groupEnd();
+
+console.group("Name and color");
+let {name, color} = point;
+console.log(name);
+console.log(color);
+console.groupEnd();
+
+console.group("Name and color different aliases");
+let {name:pointName, color:pointColor} = point;
+console.log(pointName);
+console.log(pointColor);
+console.groupEnd();
+
+console.group("Pick certain elements from array");
+var myArray = [];
+for (let i=1; i<=10; i++){
+	myArray.push(i);
+}
+let [firstItem,,,,,sixthItem] = myArray;
+console.log(firstItem);
+console.log(sixthItem);
+console.groupEnd();
+
+console.group("Pick certain elements from array and destructure their personal stuff");
+myArray = [];
+for (let i=1; i<=10; i++){
+	myArray.push({
+		id: i,
+		myStuff: _.uniqueId('Item ' + i.toString())
+	});
+}
+let [firstItem,,,,,sixthItem] = myArray;
+function getMyStuff({myStuff}){
+	console.log(myStuff);
+}
+getMyStuff(firstItem);
+getMyStuff(sixthItem);
+console.groupEnd();
